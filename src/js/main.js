c8cd7be4886680184e46060e1193ea50be8561d5
Denis Kuznetsov <kyznecov995@icloud.com>
2023-07-29 23:44:58 +0400

    create promo

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,8 @@
+import $ from 'jquery';
 import '../scss/main.scss';
 import '../index.html';
 import '../../node_modules/focus-visible/dist/focus-visible';
-import $ from 'jquery';
 
 $(function () {
-  console.log('Hello world');
+  console.log($('h1').text());
 });
-
-console.log(123);
