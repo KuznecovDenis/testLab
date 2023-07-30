c7a2ddae2a0215d9cbc136b0cdb87ba389ab426b
Denis Kuznetsov <kyznecov995@icloud.com>
2023-07-30 23:46:42 +0400

    fix bug menu

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -6,20 +6,26 @@ const menu = () => {
     $('.body').css('overflow', function (_, value) {
       return value === 'hidden' ? 'initial' : 'hidden';
     });
+
     imgsrc('.header__logo img');
   });
 
   $('.nav__item').on('click', function () {
     $('.body').css('overflow', 'initial');
-    $('.header').removeClass('open-menu');
-    imgsrc('.header__logo img');
+    if ($('.header').hasClass('open-menu')) {
+      imgsrc('.header__logo img');
+      $('.header').removeClass('open-menu');
+    }
   });
 };
 
 function imgsrc(img) {
-  if ($(img).attr('src') == './img/logo.svg')
+  console.log($(img).attr('src'));
+  if ($(img).attr('src') == './img/logo.svg') {
     $(img).attr('src', './img/logo-dark.svg');
-  else $(img).attr('src', './img/logo.svg');
+  } else {
+    $(img).attr('src', './img/logo.svg');
+  }
 }
 
 export default menu;
